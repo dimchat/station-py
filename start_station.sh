a5c72f1a5b99261f5f99efbffaa0e0a0456bda0e
Moky <albert.moky@gmail.com>
2019-02-05 12:57:27 +0800

    update script for restart station

--- start_station.sh
+++ start_station.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-ps -fe | grep "dim\/station\/start.py" | grep -v grep
+ps -fe | grep "\/srv\/dim\/station\/start.py" | grep -v grep
 if [ $? -ne 0 ]
 then
     time=`date +%Y%m%d-%H%M%S`
     stdbuf -oL /usr/local/bin/python3 /srv/dim/station/start.py >> /tmp/dims-${time}.log 2>&1 &
 else
-    echo "Station is running..." >&2
+    echo "Station is running..."
 fi
