62fdbb787ba1a858965175269614d85fa61ca06e
Moky <albert.moky@gmail.com>
2019-03-24 15:39:38 +0800

    reconnect APNs server for broken pipe

--- station/apns.py
+++ station/apns.py
@@ -42,30 +42,75 @@ class ApplePushNotificationService:
     def __init__(self, credentials, use_sandbox=False, use_alternative_port=False, proto=None, json_encoder=None,
                  password=None, proxy_host=None, proxy_port=None):
         super().__init__()
-        # APNs client
-        self.client = APNsClient(credentials=credentials, use_sandbox=use_sandbox,
-                                 use_alternative_port=use_alternative_port,
-                                 proto=proto, json_encoder=json_encoder, password=password,
-                                 proxy_host=proxy_host, proxy_port=proxy_port)
+        # APNs client parameters
+        self.credentials = credentials
+        self.use_sandbox = use_sandbox
+        self.use_alternative_port = use_alternative_port
+        self.proto = proto
+        self.json_encoder = json_encoder
+        self.password = password
+        self.proxy_host = proxy_host
+        self.proxy_port = proxy_port
+        self.client = None  # APNsClient
         # topic
         self.topic = 'chat.dim.sechat'
         # delegate to get device token
         self.delegate = None  # IAPNsDelegate
 
+    def connect(self) -> bool:
+        try:
+            self.client = APNsClient(credentials=self.credentials, use_sandbox=self.use_sandbox,
+                                     use_alternative_port=self.use_alternative_port,
+                                     proto=self.proto, json_encoder=self.json_encoder, password=self.password,
+                                     proxy_host=self.proxy_host, proxy_port=self.proxy_port)
+            return True
+        except IOError as error:
+            print('APNs: failed to connect apple server: %s' % error)
+            return False
+
+    def send_notification(self, token_hex, notification, topic=None,
+                          priority='10', expiration=None, collapse_id=None) -> int:
+        if self.client is None and self.connect() is False:
+            print('APNs: cannot connect apple server, message dropped: %s' % notification)
+            return -503  # Service Unavailable
+        try:
+            if topic is None:
+                topic = self.topic
+            # push to apple server
+            self.client.send_notification(token_hex=token_hex, notification=notification, topic=topic,
+                                          priority=priority, expiration=expiration, collapse_id=collapse_id)
+            return 200  # OK
+        except IOError as error:
+            print('APNs: connection lost: %s' % error)
+            return -408  # Request Timeout
+        except APNsException as error:
+            print('APNs: failed to push notification: %s, error %s' % (notification, error))
+            return -400  # Bad Request
+
     def push(self, identifier: str, message: str, badge: int=1) -> bool:
+        # 1. check
         tokens = self.delegate.device_tokens(identifier=identifier)
         if tokens is None:
             print('APNs: cannot get device token for user %s' % identifier)
             return False
+        # 2. send
         success = 0
         for token in tokens:
             print('APNs: sending notification %s to user %s with token %s' % (message, identifier, token))
             payload = Payload(alert=message, badge=badge, sound='default')
-            try:
-                self.client.send_notification(token_hex=token, notification=payload, topic=self.topic)
+            # first try
+            result = self.send_notification(token_hex=token, notification=payload)
+            if result == -503:  # Service Unavailable
+                # connection failed
+                return success > 0
+            elif result == -408:  # Request Timeout
+                print('APNs: Broken pipe? try to reconnect again!')
+                # reset APNs client
+                self.client = None
+                # try again
+                result = self.send_notification(token_hex=token, notification=payload)
+            if result == 200:  # OK
                 success = success + 1
-            except APNsException as error:
-                print('failed to push notification: %s, error %s' % (message, error))
         return success > 0
 
 
