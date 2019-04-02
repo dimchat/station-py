b343168a0d425545edcfe4a2d80431c697edf26e
Moky <albert.moky@gmail.com>
2019-04-02 19:22:06 +0800

    save uploaded files with identifier.address

--- fileserver/server.py
+++ fileserver/server.py
@@ -30,24 +30,30 @@
     Upload/download image files
 """
 
-import os
-import hashlib
-from binascii import b2a_hex
-import json
-
-from flask import Flask, request, send_from_directory, render_template
 from werkzeug.utils import secure_filename
+from flask import Flask, request, send_from_directory, render_template
+
+import sys
+import os
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+from fileserver.librarian import FileManager
 
 HOST = '0.0.0.0'
 PORT = 8081
 
-UPLOAD_DIRECTORY = '/tmp/www/uploads'
+# UPLOAD_DIRECTORY = '/tmp/www/uploads'
+UPLOAD_DIRECTORY = '/data/.dim/uploads'
+
+ALLOWED_FILE_TYPE = {'png', 'jpg', 'jpeg', 'gif'}
+
+file_manager = FileManager(base_dir=UPLOAD_DIRECTORY)
+file_manager.extensions = ALLOWED_FILE_TYPE
 
 app = Flask(__name__)
-
-
-def md5(data) -> str:
-    return b2a_hex(hashlib.md5(data).digest()).decode('utf-8')
 
 
 @app.route('/test.html', methods=['GET'])
@@ -55,37 +61,42 @@ def test() -> str:
     return render_template('test.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload() -> str:
+@app.route('/<string:sender>/upload', methods=['POST'])
+def upload(sender: str) -> str:
+    # 1. get file data
     file = request.files['file']
     if file is None:
-        return json.dumps({'code': -1, 'message': 'No file uploaded'})
-    # get file data
+        # 400 - Bad Request
+        return render_template('response.html', command='upload', code=400, message='Bad Request')
+    filename = secure_filename(file.filename)
     data = file.read()
     if data is None or len(data) == 0:
-        return json.dumps({'code': -2, 'message': 'File data is empty'})
-    # get file extension
-    filename = file.filename
-    if filename is None or '.' not in filename:
-        ext = 'png'
-    else:
-        ext = filename.rsplit('.', 1)[1]
-    # save file data
-    filename = '%s.%s' % (md5(data), ext)
-    path = '%s/%s' % (UPLOAD_DIRECTORY, filename)
-    with open(path, 'wb') as file:
-        file.write(data)
-    return json.dumps({'code': 0, 'message': 'Upload success!', 'filename': filename})
+        # 204 - No Content
+        return render_template('response.html', command='upload', code=204, message='No Content', filename=filename)
+    # save file
+    try:
+        file_manager.save(data=data, filename=filename, sender=sender)
+    except TypeError as error:
+        # not supported file type
+        return render_template('response.html', command='upload', code=415, message=error, filename=filename)
+    except AssertionError as error:
+        # filename or data cannot be empty
+        return render_template('response.html', command='upload', code=204, message=error, filename=filename)
+    # 200 - OK
+    return render_template('response.html', command='upload', code=200, message='OK', filename=filename)
 
 
-@app.route('/download/<path:filename>')
-def download(filename) -> str:
+@app.route('/download/<string:sender>/<path:filename>', methods=['GET'])
+def download(sender: str, filename: str) -> str:
     filename = secure_filename(filename)
-    if '.' not in filename:
-        filename = filename + '.png'
-    path = '%s/%s' % (UPLOAD_DIRECTORY, filename)
-    if os.path.isfile(path):
-        return send_from_directory(UPLOAD_DIRECTORY, filename, as_attachment=True)
+    # check file exists
+    path = file_manager.path(filename=filename, sender=sender)
+    if not os.path.isfile(path):
+        # 404 - Not Found
+        return render_template('response.html', command='download', code=404, message='Not Found', filename=filename)
+    # response file data as attachment
+    directory = file_manager.directory(sender=sender)
+    return send_from_directory(directory, filename, as_attachment=True)
 
 
 if __name__ == '__main__':
