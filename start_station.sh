6fbc0b605788ada4260906483a4d2da582ec5b74
Moky <albert.moky@gmail.com>
2019-04-03 23:05:59 +0800

    start file server

--- start_station.sh
+++ start_station.sh
@@ -8,3 +8,12 @@ then
 else
     echo "Station is running..."
 fi
+
+ps -fe | grep "\/srv\/dim\/fileserver\/server.py" | grep -v grep
+if [ $? -ne 0 ]
+then
+    time=`date +%Y%m%d-%H%M%S`
+    stdbuf -oL /usr/local/bin/python3 /srv/dim/fileserver/server.py >> /tmp/cdn-${time}.log 2>&1 &
+else
+    echo "File server is running..."
+fi
