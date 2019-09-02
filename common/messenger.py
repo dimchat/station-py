37c67b2313121000a449dcd1dd4dc73f50792a9a
Moky <albert.moky@gmail.com>
2019-09-02 23:58:17 +0800

    upgrade messenger
    1. support Meta Protocol;
    2. support Forward message;

--- common/messenger.py
+++ common/messenger.py
@@ -29,22 +29,26 @@
 
     Transform and send message
 """
+import json
 
 from dimp import Meta
 from dimp import InstantMessage, ReliableMessage, ContentType, ForwardContent
-from dimp import Transceiver, ITransceiverDelegate, ICompletionHandler
+from dimp import Transceiver, ICompletionHandler, ICallback
+from dkd import SecureMessage
 
 from .facebook import Facebook
 
 
-class Messenger(Transceiver, ITransceiverDelegate):
+class Messenger(Transceiver):
 
     def __init__(self):
         super().__init__()
-        self.delegate = self
 
-    def verify_decrypt(self, msg: ReliableMessage):
-        # 0. [Meta Protocol] check meta in first contact message
+    #
+    #  Transform
+    #
+    def verify_message(self, msg: ReliableMessage):
+        # [Meta Protocol] check meta in first contact message
         sender = self.barrack.identifier(msg.envelope.sender)
         meta = self.barrack.meta(identifier=sender)
         if meta is None:
@@ -52,16 +56,19 @@ class Messenger(Transceiver, ITransceiverDelegate):
             meta = Meta(msg.meta)
             if meta is None:
                 # TODO: query meta for sender from DIM network
-                raise LookupError('failed to get meta for sender: %s' % sender)
-            assert meta.match_identifier(identifier=sender), 'meta not match: %s, %s' % (sender, meta)
+                #       (do it by application)
+                return None
+            elif not meta.match_identifier(sender):
+                raise ValueError('meta not match: %s, %s' % (sender, meta))
+            # save meta for new contact
             facebook: Facebook = self.barrack
             if not facebook.save_meta(meta=meta, identifier=sender):
-                raise ValueError('save meta error: %s, %s' % (sender, meta))
+                raise RuntimeError('save meta error: %s, %s' % (sender, meta))
+        return super().verify_message(msg=msg)
 
-        # 1. verify and decrypt
-        i_msg = super().verify_decrypt(msg)
-
-        # 2. check: top-secret message
+    def decrypt_message(self, msg: SecureMessage):
+        i_msg = super().decrypt_message(msg=msg)
+        # check: top-secret message
         if i_msg.content.type == ContentType.Forward:
             # do it again to drop the wrapper,
             # the secret inside the content is the real message
@@ -71,18 +78,85 @@ class Messenger(Transceiver, ITransceiverDelegate):
             if secret is not None:
                 return secret
             # FIXME: not for you?
+        return i_msg
 
+    #
+    #  Conveniences
+    #
+    def encrypt_sign(self, msg: InstantMessage) -> ReliableMessage:
+        # 1. encrypt 'content' to 'data' for receiver
+        s_msg = self.encrypt_message(msg=msg)
+        # 2. sign 'data' by sender
+        r_msg = self.sign_message(msg=s_msg)
+        # OK
+        return r_msg
+
+    def verify_decrypt(self, msg: ReliableMessage) -> InstantMessage:
+        # 1. verify 'data' with 'signature'
+        s_msg = self.verify_message(msg=msg)
+        # 2. check group message
+        receiver = self.barrack.identifier(msg.envelope.receiver)
+        if receiver.type.is_group():
+            # TODO: split it
+            pass
+        # 3. decrypt 'data' to 'content'
+        i_msg = self.decrypt_message(msg=s_msg)
         # OK
         return i_msg
 
     #
-    #  ITransceiverDelegate
+    #   Send message
     #
-    def send_package(self, data: bytes, handler: ICompletionHandler) -> bool:
-        pass
+    def send_message(self, msg: InstantMessage, callback: ICallback, split: bool) -> bool:
+        """
+        Send message (secured + certified) to target station
 
-    def upload_data(self, data: bytes, msg: InstantMessage) -> str:
-        pass
+        :param msg:      instant message
+        :param callback: callback function
+        :param split:    if it's a group message, split it before sending out
+        :return:         False on data/delegate error
+        """
+        # transforming
+        r_msg = self.encrypt_sign(msg=msg)
+        if r_msg is None:
+            raise AssertionError('failed to encrypt and sign message: %s' % msg)
+        # trying to send out
+        ok = True
+        receiver = self.barrack.identifier(msg.envelope.receiver)
+        if split and receiver.type.is_group():
+            group = self.barrack.group(identifier=receiver)
+            if group is None:
+                raise LookupError('failed to create group: %s' % receiver)
+            messages = r_msg.split(members=group.members)
+            if messages is None:
+                # failed to split msg, send it to group
+                ok = self.__send_message(msg=r_msg, callback=callback)
+            else:
+                # sending group message one by one
+                for r_msg in messages:
+                    if not self.__send_message(msg=r_msg, callback=callback):
+                        ok = False
+        else:
+            ok = self.__send_message(msg=r_msg, callback=callback)
+        # TODO: if OK, set iMsg.state = sending; else set iMsg.state = waiting
+        return ok
 
-    def download_data(self, url: str, msg: InstantMessage) -> bytes:
-        pass
+    def __send_message(self, msg: ReliableMessage, callback: ICallback) -> bool:
+        data = json.dumps(msg).encode('utf-8')
+        handler = CompletionHandler(msg=msg, cb=callback)
+        assert self.delegate is not None, 'messenger delegate not set yet'
+        return self.delegate.send_package(data=data, handler=handler)
+
+
+class CompletionHandler(ICompletionHandler):
+
+    def __init__(self, msg: ReliableMessage, cb: ICallback):
+        super().__init__()
+        self.msg = msg
+        self.callback = cb
+
+    def success(self):
+        self.callback.finished(result=self.msg, error=None)
+
+    def failed(self, error):
+        self.callback.finished(result=self.msg, error=error)
