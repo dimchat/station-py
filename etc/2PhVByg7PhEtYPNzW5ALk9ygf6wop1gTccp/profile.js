00abc8ae01703b73604eda9620de7c7d25986a20
Moky <albert.moky@gmail.com>
2019-10-18 20:31:19 +0800

    avatar for xiaoxiao

--- etc/2PhVByg7PhEtYPNzW5ALk9ygf6wop1gTccp/profile.js
+++ etc/2PhVByg7PhEtYPNzW5ALk9ygf6wop1gTccp/profile.js
@@ -1,4 +1,5 @@
 {
     "ID": "xiaoxiao@2PhVByg7PhEtYPNzW5ALk9ygf6wop1gTccp",
-    "name": "小小"
+    "name": "小小",
+    "avatar": "https://sechat.dim.chat/avatar/2PhVByg7PhEtYPNzW5ALk9ygf6wop1gTccp/avatar.png"
 }
