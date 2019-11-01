1b7a97cd9b5e6ac2e45417e0171661c2bc9933c8
Moky <albert.moky@gmail.com>
2019-11-01 23:18:23 +0800

    www server test OK

---
+++ webserver/config.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+"""
+    Web Server Config
+    ~~~~~~~~~~~~~~~~~
+
+    Configuration for WWW
+"""
+
+from dimsdk import AddressNameService
+from dimsdk import KeyStore, Messenger
+
+#
+#  Common Libs
+#
+from libs.common import Log
+from libs.common import Database, Facebook
+
+from libs.common.immortals import moki_id, moki_sk, moki_meta, moki_profile
+from libs.common.immortals import hulk_id, hulk_sk, hulk_meta, hulk_profile
+
+#
+#  Configurations
+#
+from etc.cfg_db import base_dir
+
+"""
+    Key Store
+    ~~~~~~~~~
+
+    Memory cache for reused passwords (symmetric key)
+"""
+g_keystore = KeyStore()
+
+"""
+    Database
+    ~~~~~~~~
+
+    for cached messages, profile manage(Barrack), reused symmetric keys(KeyStore)
+"""
+g_database = Database()
+g_database.base_dir = base_dir
+Log.info("database directory: %s" % g_database.base_dir)
+
+"""
+    Facebook
+    ~~~~~~~~
+
+    Barrack for cache entities
+"""
+g_facebook = Facebook()
+g_facebook.database = g_database
+
+"""
+    Address Name Service
+    ~~~~~~~~~~~~~~~~~~~~
+
+    A map for short name to ID, just like DNS
+"""
+g_ans = AddressNameService()
+g_ans.database = g_database
+
+"""
+    Messenger
+    ~~~~~~~~~
+"""
+g_messenger = Messenger()
+g_messenger.barrack = g_facebook
+g_messenger.key_cache = g_keystore
+
+
+def load_immortals():
+    # load immortals
+    Log.info('immortal user: %s' % moki_id)
+    g_facebook.save_meta(identifier=moki_id, meta=moki_meta)
+    g_facebook.save_private_key(identifier=moki_id, private_key=moki_sk)
+    g_facebook.save_profile(profile=moki_profile)
+
+    Log.info('immortal user: %s' % hulk_id)
+    g_facebook.save_meta(identifier=hulk_id, meta=hulk_meta)
+    g_facebook.save_private_key(identifier=hulk_id, private_key=hulk_sk)
+    g_facebook.save_profile(profile=hulk_profile)
+
+
+"""
+    Loading info
+    ~~~~~~~~~~~~
+"""
+
+# load immortal accounts
+Log.info('-------- loading immortals accounts')
+load_immortals()
+
+Log.info('======== configuration OK!')
---
+++ webserver/httpd.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+"""
+    Web Server
+    ~~~~~~~~~~
+
+    1. Query meta, profile
+    2. Verify message
+"""
+
+import sys
+import os
+from binascii import Error
+
+from flask import Flask, jsonify, render_template, request
+
+from dimp import ID, Meta, Profile
+from dimp import MetaCommand, ProfileCommand
+from mkm.crypto.utils import base64_decode
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+sys.path.append(os.path.join(rootPath, 'libs'))
+
+from libs.common import Log
+from webserver.config import g_facebook
+
+
+class Worker:
+
+    def info(self, msg: str):
+        Log.info('%s:\t%s' % (self.__class__.__name__, msg))
+
+    def error(self, msg: str):
+        Log.error('%s ERROR:\t%s' % (self.__class__.__name__, msg))
+
+    def identifier(self, identifier: str) -> ID:
+        try:
+            return g_facebook.identifier(string=identifier)
+        except ValueError:
+            self.error('ID error: %s' % identifier)
+
+    def meta(self, identifier: ID) -> Meta:
+        info = g_facebook.meta(identifier=identifier)
+        if info is None:
+            self.info('meta not found: %s' % identifier)
+        else:
+            return info
+
+    def profile(self, identifier: ID) -> Profile:
+        info = g_facebook.profile(identifier=identifier)
+        if info is None:
+            self.info('profile not found: %s' % identifier)
+        else:
+            return info
+
+    def decode_data(self, data: str) -> bytes:
+        try:
+            return base64_decode(data)
+        except Error:
+            self.error('data not base64: %s' % data)
+
+    def decode_signature(self, signature: str) -> bytes:
+        try:
+            return base64_decode(signature)
+        except Error:
+            self.error('signature not base64: %s' % signature)
+
+
+g_worker = Worker()
+
+
+"""
+    WWW Config
+"""
+WWW_HOST = '0.0.0.0'
+WWW_PORT = 8384
+
+BASE_URI = '/dimp/v1'
+
+app = Flask(__name__)
+
+
+@app.route(BASE_URI+'/test.html', methods=['GET'])
+def test() -> str:
+    return render_template('test.html')
+
+
+@app.route(BASE_URI+'/meta/<string:identifier>', methods=['GET'])
+def query_meta(identifier: str) -> str:
+    # check ID
+    identifier = g_worker.identifier(identifier)
+    if identifier is None:
+        response = {
+            'code': 400,  # Bad Request
+            'message': 'ID error',
+        }
+    else:
+        # get meta
+        meta = g_worker.meta(identifier=identifier)
+        if meta is None:
+            response = {
+                'code': 404,
+                'message': 'Meta not found',
+            }
+        else:
+            # response OK
+            cmd = MetaCommand.new(identifier=identifier, meta=meta)
+            response = {
+                'code': 200,
+                'message': 'OK',
+                'content': cmd,
+            }
+    return jsonify(response)
+
+
+@app.route(BASE_URI+'/profile/<string:identifier>', methods=['GET'])
+def query_profile(identifier: str) -> str:
+    # check ID
+    identifier = g_worker.identifier(identifier)
+    if identifier is None:
+        response = {
+            'code': 400,  # Bad Request
+            'message': 'ID error',
+        }
+    else:
+        # get profile
+        profile = g_worker.profile(identifier=identifier)
+        if profile is None:
+            response = {
+                'code': 404,
+                'message': 'Profile not found',
+            }
+        else:
+            # get meta
+            meta = g_worker.meta(identifier=identifier)
+            # response OK
+            cmd = ProfileCommand.new(identifier=identifier, meta=meta, profile=profile)
+            response = {
+                'code': 200,
+                'message': 'OK',
+                'content': cmd,
+            }
+    return jsonify(response)
+
+
+@app.route(BASE_URI+'/verify', methods=['POST'])
+def verify_message() -> str:
+    # post data
+    form = request.form.to_dict()
+    sender = form.get('sender')
+    data = form.get('data')
+    signature = form.get('signature')
+    # check ID
+    identifier = g_worker.identifier(sender)
+    if identifier is None:
+        response = {
+            'code': 400,  # Bad Request
+            'message': 'ID error',
+        }
+    else:
+        # get meta
+        meta = g_worker.meta(identifier=identifier)
+        if meta is None:
+            response = {
+                'code': 404,
+                'message': 'Meta not found',
+            }
+        else:
+            # check signature with data
+            data = g_worker.decode_data(data)
+            signature = g_worker.decode_signature(signature)
+            if data is None or signature is None:
+                response = {
+                    'code': 412,  # Precondition Failed
+                    'message': 'Data or signature error',
+                }
+            elif meta.key.verify(data=data, signature=signature):
+                response = {
+                    'code': 200,
+                    'message': 'OK',
+                }
+            else:
+                response = {
+                    'code': 403,  # Forbidden
+                    'message': 'Signature not match',
+                }
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(host=WWW_HOST, port=WWW_PORT, debug=True)
