56526281561e7e3d438d71893ef9ae45973964c3
Moky <albert.moky@gmail.com>
2019-11-11 19:38:05 +0800

    parameters for public keys

--- etc/7ThVZeDuQAdG3eSDF6NeFjMDPjKN5SbrnM/meta.js
+++ etc/7ThVZeDuQAdG3eSDF6NeFjMDPjKN5SbrnM/meta.js
@@ -3,7 +3,10 @@
     "seed": "Group-Naruto",
     "key": {
         "algorithm": "RSA",
-        "data": "-----BEGIN PUBLIC KEY-----\nMIGJAoGBAJk2/bfJwunI9iuNBb3kJ8HCBfh73+TR+qerqE4K4Ew5lU23MmMIAE3Z\natKzGkMsbdMDzQ5EeHEoHPIQHWRoI1/cR6tN++XzjqYYBIMoLY58q8S7T65UNkgb\nJWf1418lraCEjh0y2fxGX55zG8VD+UaOb4qpar4IMZ6PhmUPIK5VAgMBAAE=\n-----END PUBLIC KEY-----\n"
+        "data": "-----BEGIN PUBLIC KEY-----\nMIGJAoGBAJk2/bfJwunI9iuNBb3kJ8HCBfh73+TR+qerqE4K4Ew5lU23MmMIAE3Z\natKzGkMsbdMDzQ5EeHEoHPIQHWRoI1/cR6tN++XzjqYYBIMoLY58q8S7T65UNkgb\nJWf1418lraCEjh0y2fxGX55zG8VD+UaOb4qpar4IMZ6PhmUPIK5VAgMBAAE=\n-----END PUBLIC KEY-----",
+        "mode": "ECB",
+        "padding": "PKCS1",
+        "digest": "SHA256"
     },
     "fingerprint": "eDL0OAK9hFksl7lH9O4regdZhn3zep3CN2xEgifcAT3F0I8mbS2Zoev9oBPULXZJd+r0l0Hyy6OWwQQGXd2yGIK67XRQiZqpna2hCq4LKSwIPWiv5/ywVgNBRxQasEwg0SeNlyXSbmCvWMefSrqNFdHz6ivNANoEQ1XEHtGwfWU="
 }
