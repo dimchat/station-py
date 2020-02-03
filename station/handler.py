9a9ac60eac340e7d3a422d7157f2b3776f8fb3b4
Moky <albert.moky@gmail.com>
2020-02-04 03:53:58 +0800

    support WebSocket now!

--- station/handler.py
+++ station/handler.py
@@ -30,7 +30,9 @@
     Handler for each connection
 """
 
+import hashlib
 import json
+import struct
 from socketserver import BaseRequestHandler
 from typing import Optional
 
@@ -39,7 +41,7 @@ from dimp import InstantMessage, ReliableMessage
 from dimsdk import NetMsgHead, NetMsg, CompletionHandler
 from dimsdk import MessengerDelegate
 
-from libs.common import Log
+from libs.common import Log, base64_encode
 from libs.server import Session
 from libs.server import ServerMessenger
 from libs.server import HandshakeDelegate
@@ -55,6 +57,9 @@ class RequestHandler(BaseRequestHandler, MessengerDelegate, HandshakeDelegate):
         super().__init__(request=request, client_address=client_address, server=server)
         # messenger
         self.__messenger: ServerMessenger = None
+        # handlers with Protocol
+        self.process_package = None
+        self.push_data = None
 
     def info(self, msg: str):
         Log.info('%s >\t%s' % (self.__class__.__name__, msg))
@@ -102,8 +107,10 @@ class RequestHandler(BaseRequestHandler, MessengerDelegate, HandshakeDelegate):
     #
     #
     def setup(self):
-        address = self.client_address
         self.__messenger: ServerMessenger = None
+        self.process_package = None
+        self.push_data = None
+        address = self.client_address
         self.info('set up with %s [%s]' % (address, station_name))
         g_session_server.set_handler(client_address=address, request_handler=self)
         g_monitor.report(message='Client connected %s [%s]' % (address, station_name))
@@ -137,126 +144,168 @@ class RequestHandler(BaseRequestHandler, MessengerDelegate, HandshakeDelegate):
         while current_station.running:
             # receive all data
             incomplete_length = len(data)
-            while True:
-                part = self.receive(1024)
-                if part is None:
-                    break
-                data += part
-                if len(part) < 1024:
-                    break
+            data = self.receive()
             if len(data) == incomplete_length:
                 self.info('no more data, exit (%d, %s)' % (incomplete_length, self.client_address))
                 break
+
+            # check protocol
+            while self.process_package is None:
+                # (Protocol A) Web socket?
+                if data.find(b'Sec-WebSocket-Key') > 0:
+                    self.process_package = self.process_ws_handshake
+                    self.push_data = self.push_ws_data
+                    break
+
+                # (Protocol B) Tencent mars?
+                try:
+                    head = NetMsgHead(data=data)
+                    if head.version == 200:
+                        # OK, it seems be a mars package!
+                        self.process_package = self.process_mars_package
+                        self.push_data = self.push_mars_data
+                        break
+                except ValueError:
+                    # self.error('not mars message pack: %s' % error)
+                    pass
+
+                # (Protocol C) raw data (JSON in line)?
+                if data.startswith(b'{"') and data.find(b'\0') < 0:
+                    self.process_package = self.process_raw_package
+                    self.push_data = self.push_raw_data
+                    break
+
+                # unknown protocol
+                data = b''
+                # raise AssertionError('unknown protocol')
+                break
+            if self.process_package is None:
+                continue
 
             # process package(s) one by one
             #    the received data packages maybe spliced,
             #    if the message data was wrap by other transfer protocol,
             #    use the right split char(s) to split it
-            while len(data) > 0:
-
-                # (Protocol A) Tencent mars?
-                mars = False
-                head = None
-                try:
-                    head = NetMsgHead(data=data)
-                    if head.version == 200:
-                        # OK, it seems be a mars package!
-                        mars = True
-                        self.push_data = self.push_mars_data
-                except ValueError:
-                    # self.error('not mars message pack: %s' % error)
-                    pass
-                # check mars head
-                if mars:
-                    self.info('@@@ msg via mars, len: %d+%d' % (head.head_length, head.body_length))
-                    # check completion
-                    pack_len = head.head_length + head.body_length
-                    if pack_len > len(data):
-                        # partially data, keep it for next loop
-                        break
-                    # cut out the first package from received data
-                    pack = data[:pack_len]
-                    data = data[pack_len:]
-                    # process mars data package
-                    response = self.process_mars_package(pack)
-                    self.send(response)
-                    # mars OK
-                    continue
-
-                if data.startswith(b'\n'):
-                    # NOOP: heartbeat package
-                    self.info('trim <heartbeats>: %s' % data)
-                    data = data.lstrip(b'\n')
-                    self.send(b'\n')
-                    continue
-
-                # (Protocol B) raw data with no wrap?
-                if data.startswith(b'{"') and data.find(b'\0') < 0:
-                    # OK, it seems be a raw package!
-                    self.push_data = self.push_raw_data
-                    # check completion
-                    pos = data.find(b'\n')
-                    if pos < 0:
-                        # partially data, keep it for next loop
-                        break
-                    # cut out the first package from received data
-                    pack = data[:pos+1]
-                    data = data[pos+1:]
-                    # process raw data package
-                    response = self.process_package(pack)
-                    self.send(response + b'\n')
-                    # raw data OK
-                    continue
-
-                # (Protocol ?)
-                # TODO: split and unwrap data package(s)
-                self.error('unknown protocol %s' % data)
-                data = b''
-                # raise AssertionError('unknown protocol')
+            data = self.process_package(data)
 
     #
-    #   process package with mars format
+    #   Protocol: WebSocket
+    #
+    ws_magic = b'258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+    ws_prefix = b'HTTP/1.1 101 Switching Protocol\r\n' \
+                b'Server: DIM-Station\r\n' \
+                b'Upgrade: websocket\r\n' \
+                b'Connection: Upgrade\r\n' \
+                b'WebSocket-Protocol: dimchat\r\n' \
+                b'Sec-WebSocket-Accept: '
+    ws_suffix = b'\r\n\r\n'
+
+    def process_ws_handshake(self, pack: bytes):
+        pos1 = pack.find(b'Sec-WebSocket-Key:')
+        pos1 += len('Sec-WebSocket-Key:')
+        pos2 = pack.find(b'\r\n', pos1)
+        key = pack[pos1:pos2].strip()
+        sec = hashlib.sha1(key + self.ws_magic).digest()
+        sec = base64_encode(sec)
+        res = self.ws_prefix + bytes(sec, 'UTF-8') + self.ws_suffix
+        self.send(res)
+        self.process_package = self.process_ws_package
+        return b''
+
+    def process_ws_package(self, pack: bytes):
+        msg_len = pack[1] & 127
+        if msg_len == 126:
+            mask = pack[4:8]
+            content = pack[8:]
+        elif msg_len == 127:
+            mask = pack[10:14]
+            content = pack[14:]
+        else:
+            mask = pack[2:6]
+            content = pack[6:]
+        data = ''
+        for i, d in enumerate(content):
+            data += chr(d ^ mask[i % 4])
+        res = self.received_package(bytes(data, 'UTF-8'))
+        self.push_ws_data(res)
+        return b''
+
+    def push_ws_data(self, body: bytes) -> bool:
+        head = struct.pack('B', 129)
+        msg_len = len(body)
+        if msg_len < 126:
+            head += struct.pack('B', msg_len)
+        elif msg_len <= (2 ** 16 - 1):
+            head += struct.pack('!BH', 126, msg_len)
+        elif msg_len <= (2 ** 64 - 1):
+            head += struct.pack('!BQ', 127, msg_len)
+        else:
+            raise ValueError('message is too long: %d' % msg_len)
+        return self.send(head + body)
+
+    #
+    #   Protocol: Tencent mars
     #
     def process_mars_package(self, pack: bytes):
-        pack = NetMsg(pack)
-        head = pack.head
+        mars = NetMsg(pack)
+        head = mars.head
+        # check completion
+        mars_len = head.head_length + head.body_length
+        pack_len = len(pack)
+        if mars_len > pack_len:
+            # partially data, keep it for next loop
+            return pack
         if head.cmd == 3:
             # TODO: handle SEND_MSG request
             if head.body_length == 0:
                 raise ValueError('messages not found')
-            # maybe more than one message in a pack
-            lines = pack.body.splitlines()
-            body = b''
-            for line in lines:
-                if line.isspace():
-                    self.info('ignore empty message')
-                    continue
-                res = self.process_package(line)
-                body = body + res + b'\n'
-            return NetMsg(cmd=head.cmd, seq=head.seq, body=body)
+            body = self.received_package(mars.body)
+            res = NetMsg(cmd=head.cmd, seq=head.seq, body=body)
         elif head.cmd == 6:
             # TODO: handle NOOP request
             self.info('receive NOOP package, response %s' % pack)
-            return pack
+            res = pack
         else:
             # TODO: handle Unknown request
             self.error('cmd=%d, seq=%d, package: %s' % (head.cmd, head.seq, pack))
-            return pack
+            res = pack
+        self.send(res)
+        # return the remaining incomplete package
+        return pack[mars_len:]
 
-    #
-    #   push message
-    #
     def push_mars_data(self, body: bytes) -> bool:
         # kPushMessageCmdId = 10001
         # PUSH_DATA_TASK_ID = 0
         data = NetMsg(cmd=10001, seq=0, body=body)
         return self.send(data)
 
+    #
+    #   Protocol: raw data (JSON string)
+    #
+    def process_raw_package(self, pack: bytes):
+        pack_len = len(pack)
+        pos = 0
+        # skip leading empty packages
+        while pack[pos] == b'\n' or pack[pos] == b' ':
+            pos += 1
+        if pos == pack_len:
+            # NOOP: heartbeat package
+            self.info('respond <heartbeats>: %s' % pack)
+            self.send(b'\n')
+            return b''
+        # check whether contain incomplete message
+        pos = pack.rfind(b'\n')
+        if pos < 0:
+            return pack
+        # maybe more than one message in a time
+        res = self.received_package(pack[:pos])
+        self.send(res)
+        # return the remaining incomplete package
+        return pack[pos+1:]
+
     def push_raw_data(self, body: bytes) -> bool:
         data = body + b'\n'
         return self.send(data=data)
-
-    push_data = push_raw_data
 
     def push_message(self, msg: ReliableMessage) -> bool:
         data = json.dumps(msg)
@@ -264,29 +313,49 @@ class RequestHandler(BaseRequestHandler, MessengerDelegate, HandshakeDelegate):
         return self.push_data(body=body)
 
     #
-    #   receive message
+    #   receive message(s)
     #
-    def process_package(self, pack: bytes) -> Optional[bytes]:
-        try:
-            res = self.messenger.received_package(data=pack)
-            if res is None:
-                # station MUST respond something to client request
-                return b''
-            return res
-        except Exception as error:
-            self.error('parse message failed: %s' % error)
-            # from dimsdk import TextContent
-            # return TextContent.new(text='parse message failed: %s' % error)
-            return b''
+    def received_package(self, pack: bytes) -> Optional[bytes]:
+        lines = pack.splitlines()
+        body = b''
+        for line in lines:
+            line = line.strip()
+            if len(line) == 0:
+                self.info('ignore empty message')
+                continue
+            try:
+                res = self.messenger.received_package(data=pack)
+                if res is None:
+                    # station MUST respond something to client request
+                    res = b''
+                else:
+                    res = res + b'\n'
+            except Exception as error:
+                self.error('parse message failed: %s' % error)
+                # from dimsdk import TextContent
+                # return TextContent.new(text='parse message failed: %s' % error)
+                res = b''
+            body = body + res
+        # all responses in one package
+        return body
 
     #
     #   Socket IO
     #
-    def receive(self, buffer_size=1024) -> bytes:
-        try:
-            return self.request.recv(buffer_size)
-        except IOError as error:
-            self.error('failed to receive data %s' % error)
+    def receive(self) -> bytes:
+        data = b''
+        while True:
+            try:
+                part = self.request.recv(1024)
+            except IOError as error:
+                self.error('failed to receive data %s' % error)
+                part = None
+            if part is None:
+                break
+            data += part
+            if len(part) < 1024:
+                break
+        return data
 
     def send(self, data: bytes) -> bool:
         try:
