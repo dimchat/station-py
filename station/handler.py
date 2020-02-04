199cc29ae308d30f26a9bd5590a451955e6da965
Moky <albert.moky@gmail.com>
2020-02-05 04:08:03 +0800

    split message packages

--- station/handler.py
+++ station/handler.py
@@ -324,7 +324,7 @@ class RequestHandler(BaseRequestHandler, MessengerDelegate, HandshakeDelegate):
                 self.info('ignore empty message')
                 continue
             try:
-                res = self.messenger.received_package(data=pack)
+                res = self.messenger.received_package(data=line)
                 if res is None:
                     # station MUST respond something to client request
                     res = b''
