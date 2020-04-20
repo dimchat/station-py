69c7189a5f608a9bcd20d38a15d7b8cfa976bd80
Moky <albert.moky@gmail.com>
2020-04-20 17:25:25 +0800

    respond nothing for 'login' in client

--- libs/client/cpu/login.py
+++ libs/client/cpu/login.py
@@ -35,59 +35,20 @@ from typing import Optional
 from dimp import ID
 from dimp import ReliableMessage
 from dimp import Content, Command
-from dimsdk import LoginCommand, ReceiptCommand
+from dimsdk import LoginCommand
 from dimsdk import CommandProcessor
-
-from ...common import Log, Database
 
 
 class LoginCommandProcessor(CommandProcessor):
-
-    def info(self, msg: str):
-        Log.info('%s >\t%s' % (self.__class__.__name__, msg))
-
-    def error(self, msg: str):
-        Log.error('%s >\t%s' % (self.__class__.__name__, msg))
-
-    @property
-    def database(self) -> Database:
-        return self.get_context('database')
-
-    @property
-    def receptionist(self):
-        return self.get_context('receptionist')
-
-    def __roaming(self, cmd: LoginCommand, sender: ID) -> Optional[ID]:
-        # check time expires
-        old = self.database.login_command(identifier=sender)
-        if old is not None:
-            if cmd.time < old.time:
-                return None
-        station = self.get_context('station')
-        assert station is not None, 'current station not in the context'
-        # get station ID
-        assert cmd.station is not None, 'login command error: %s' % cmd
-        sid = self.facebook.identifier(cmd.station.get('ID'))
-        if sid == station.identifier:
-            return None
-        return sid
 
     #
     #   main
     #
     def process(self, content: Content, sender: ID, msg: ReliableMessage) -> Optional[Content]:
         assert isinstance(content, LoginCommand), 'command error: %s' % content
-        # check roaming
-        sid = self.__roaming(cmd=content, sender=sender)
-        if sid is not None:
-            self.info('%s roamed to: %s' % (sender, sid))
-            self.receptionist.add_roamer(identifier=sender)
-        # update login info
-        if not self.database.save_login(cmd=content, sender=sender, msg=msg):
-            return None
-        # response
-        self.info('login command: %s' % content)
-        return ReceiptCommand.new(message='Login received')
+        # self.info('login command: %s' % content)
+        # return ReceiptCommand.new(message='Login received')
+        return None
 
 
 # register
