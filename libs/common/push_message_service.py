b8ae1b1071e673d55084591a5d5e73aa7a56c94f
John Chen <john.chen@infothinker.com>
2020-08-13 16:33:24 +0800

    push service

--- libs/common/push_message_service.py
+++ libs/common/push_message_service.py
@@ -1,20 +1,19 @@
+import traceback
 import pika
-from pika import BlockingConnection
 import json
 
 
 class PushMessageService:
 
-    connection: BlockingConnection
     queue_key = "dim_push_message"
-
-    def __init__(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue_key)
 
     def push(self, sender: str, receiver: str, message: str):
         try:
+
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+            channel = connection.channel()
+            channel.queue_declare(queue=self.queue_key)
+
             json_dict = {
                 "from": sender,
                 "to": receiver,
@@ -24,6 +23,15 @@ class PushMessageService:
 
             json_str = json.dumps(json_dict)
             print("Now publish to rabbitmq {}".format(json_str))
-            self.channel.basic_publish(exchange='', routing_key=self.queue_key, body=json_str)
+            channel.basic_publish(exchange='', routing_key=self.queue_key, body=json_str)
+
         except:
-            pass
+
+            exception_str = traceback.format_exc()
+            print("Push exception happen {}".format(exception_str))
+
+
+if __name__ == "__main__":
+
+    pusher = PushMessageService()
+    pusher.push("pony@4TnzoxrZSPVwFg7hmK7W12Wh1iu3hGz5G5", "dim@4MVvC3bTTYqozq4XvXVMt5VSWLyLK1XSVg", "Hello")
